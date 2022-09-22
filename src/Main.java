9e9ceeeffb0ed76e09a38a43b07aaad90d905990
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-09-22 10:17:10 +0200

    main -random licitálás

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     private static List<Festmeny> festmenyek;
+
     public static void main(String[] args) {
         System.out.println(felhasznaloBevitel());
         try {
             readInFile();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.err.println("Nem található a fájl!");
-        }catch (IOException e){
+        } catch (IOException e) {
             System.err.println("Hiba történt a fájl beolvasása közben!");
         }
         for (Festmeny f : festmenyek) {
             System.out.println(f);
         }
     }
-    public static List<Festmeny> felhasznaloBevitel(){
+
+    public static List<Festmeny> felhasznaloBevitel() {
         List<Festmeny> festmenyek = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
         System.out.print("Hány darab festményt szeretne hozzáadni?");
@@ -35,14 +35,16 @@ public class Main {
             Festmeny f = new Festmeny(splited[0], splited[1], splited[2]);
             festmenyek.add(f);
         }
+        randomLicit();
         return festmenyek;
     }
+
     public static void readInFile() throws IOException {
         FileReader fr = new FileReader("festmenyek.csv");
-        festmenyek = new LinkedList<>();
+        festmenyek = new ArrayList<>();
         BufferedReader br = new BufferedReader(fr);
         String sor = br.readLine();
-        while(sor != null){
+        while (sor != null) {
             String[] splitted = sor.split(";");
             Festmeny f = new Festmeny(splitted[1], splitted[0], splitted[2]);
             festmenyek.add(f);
@@ -50,5 +52,12 @@ public class Main {
         }
         br.close();
         fr.close();
+    }
+
+    public static void randomLicit() {
+        Random rnd = new Random();
+        for (int i = 0; i < 20; i++) {
+            festmenyek.get(rnd.nextInt(festmenyek.size())).licit(rnd.nextInt(91) + 10);
+        }
     }
 }
