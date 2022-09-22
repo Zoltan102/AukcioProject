2c78945ff6fe02fe9d40f0094bc0763ce95a1795
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-09-22 09:49:07 +0200

    main -fájl beolvasás

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    private static List<Festmeny> festmenyek;
     public static void main(String[] args) {
         System.out.println(felhasznaloBevitel());
+        try {
+            readInFile();
+        }catch (FileNotFoundException e){
+            System.err.println("Nem található a fájl!");
+        }catch (IOException e){
+            System.err.println("Hiba történt a fájl beolvasása közben!");
+        }
     }
     public static List<Festmeny> felhasznaloBevitel(){
         List<Festmeny> festmenyek = new LinkedList<>();
@@ -21,5 +34,18 @@ public class Main {
         }
         return festmenyek;
     }
-    public
+    public static void readInFile() throws IOException {
+        FileReader fr = new FileReader("festmenyek.csv");
+        festmenyek = new LinkedList<>();
+        BufferedReader br = new BufferedReader(fr);
+        String sor = br.readLine();
+        while(sor != null && sor.equals("")){
+            String[] splitted = sor.split(";");
+            Festmeny f = new Festmeny(splitted[1], splitted[0], splitted[2]);
+            festmenyek.add(f);
+            sor = br.readLine();
+        }
+        br.close();
+        fr.close();
+    }
 }
